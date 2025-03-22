7a6cc86e75322d8cc4d9ddb3d71ca54e369ee49f
ksu4110e113 <ksu4110e113@gmail.com>
2025-03-22 11:19:39 +0800

    新增 /menu API 回傳測試資料

--- main.py
+++ main.py
@@ -2,6 +2,17 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+from pydantic import BaseModel
+
+class MenuItem(BaseModel):
+    name: str
+    price: int
+    intro: str
+
+
 @app.get("/")
 def read_root():
-    return {"message": "Hello FastAPI!"}
+    return [
+    {"name": "炸雞", "price": 120, "intro": "酥脆多汁"},
+    {"name": "珍奶", "price": 60, "intro": "香濃黑糖 Q 彈珍珠"}
+    ]
